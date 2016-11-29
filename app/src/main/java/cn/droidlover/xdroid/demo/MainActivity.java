d7c39b1553470f8d83bf5b23cfd61f0a9fd06556
wanglei <droidlover@126.com>
2016-11-29 12:45:17 +0800

    add log

---
+++ app/src/main/java/cn/droidlover/xdroid/demo/MainActivity.java
@@ -0,0 +1,10 @@
+package cn.droidlover.xdroid.demo;
+
+import cn.droidlover.xdroid.base.XActivity;
+
+/**
+ * Created by wanglei on 2016/11/29.
+ */
+
+public class MainActivity extends XActivity{
+}
